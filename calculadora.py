f11a87d28de902a7ea5f10b9c739cc1504780138
Laryssa Finizola <163913709+laryssa-finizola@users.noreply.github.com>
2025-08-20 21:04:08 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+num1 = float(input("numero 1:"))
+num2 = float(input("numero 2:"))
+
+soma = num1 + num2
+subtrai = num1 - num2
+multiplica = num1 * num2    
+divide = num1 / num2
+resto_divisao = num1 % num2
+
+print ("soma:", soma)
+print ("subtração:", subtrai)
+print ("multiplica:", multiplica)
+print ("divide:", divide)
+print ("resto da divisão:", resto_divisao)
