c29da6217fab379dfb87ac741446da06bf895bcc
Laryssa Finizola <163913709+laryssa-finizola@users.noreply.github.com>
2025-08-25 21:13:56 -0300

    Add files via upload

---
+++ mensagem.py
@@ -0,0 +1,13 @@
+numero= int(input("Digite um número:"))
+
+if numero == 1:
+    print("Bom dia!")
+
+elif numero == 2:
+    print("Boa tarde!")
+
+elif numero == 3:
+    print("Boa noite!")
+
+else:
+    print("Erro, número inválido")
