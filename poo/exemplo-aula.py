990190ebe35e5bf0d779514faea99fc06b360e38
laryssa-finizola <laryssafinizoladev@gmail.com>
2025-09-29 20:24:18 -0300

    exemplo feito em aula

--- poo/exemplo-aula.py
+++ poo/exemplo-aula.py
@@ -8,7 +8,8 @@ class Livro:
 
     def detalhes(self):
         """Retorna uma string formatada com as informações do livro."""
-        return f"Título: '{self.titulo}' | Autor: {self.autor} | ISBN: {self.isbn}" #podemos usar o return
+        return f"Título: '{self.titulo}' | Autor: {self.autor} | ISBN: {self.isbn}"
+        #podemos usar o return
         #print(f"Título: '{self.titulo}' | Autor: {self.autor} | ISBN: {self.isbn}")
 
 # --- PRÁTICA ---
@@ -18,7 +19,6 @@ livro2 = Livro("O Alquimista", "Paulo Coelho", "978-85-00-00000-0") #objeto2 liv
 print("--- Livros Criados ---")
 print(livro1.detalhes())
 print(livro2.detalhes())
-
 
 #quando lidamos com métodos e funções eles sempre retornam algo e  no lugar de usar prints dentro deles,
 # nós usamos o 'return' que no retornará o que colocamos no algoritmo
